7bed3f3b9fa2fade0c84e4a70f6a9da7bd469e08
yeekun <44221007+yeekun@users.noreply.github.com>
2018-12-16 09:41:24 +0800

    change junit code

--- Documents/NetBeansProjects/JLS/test/jls/Kun/FreshTest.java
+++ Documents/NetBeansProjects/JLS/test/jls/Kun/FreshTest.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jls.Kun;
 
 import ADT.LList;
-import java.util.ArrayList;
 import jls.Product;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -18,45 +10,17 @@ import static org.junit.Assert.*;
  *
  * @author Yee Kun
  */
-
 public class FreshTest {
     LList<Product> ProductList = new LList<>();
     Fresh f = new Fresh(ProductList);
     
     String item;
-   
-    public FreshTest() {
-    }
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
+    public FreshTest() { 
     }
     
     @Before
     public void setUp() {
-        
-       
-    }
-
-    /**
-     * Test of ListItem method, of class Fresh.
-     */
-    @Test
-    public void testListItem() {
-        
-        System.out.println("ListItem");
-        Fresh instance = new Fresh(ProductList);
-        for(int i=1; i<ProductList.getNumberOfEntries(); i++){
-            String item = ProductList.getEntry(i).getName();
-            assertEquals("Rose", item);
-        }
-        
-        // TODO review the generated test code and remove the default call to fail.
-      //  fail("The test case is a prototype.");
     }
 
     /**
@@ -66,9 +30,10 @@ public class FreshTest {
     public void testAddRowToJTable() {
         System.out.println("addRowToJTable");
         Fresh instance = new Fresh(ProductList);
-        instance.addRowToJTable();
-        // TODO review the generated test code and remove the default call to fail.
-       // fail("The test case is a prototype.");
+        for(int i=1; i<ProductList.getNumberOfEntries(); i++){
+            String item = ProductList.getEntry(i).getName();
+            assertEquals("Rose", item);
+        }
     }
     
 }
--- Documents/NetBeansProjects/JLS/test/jls/Kun/SelectItemTest.java
+++ Documents/NetBeansProjects/JLS/test/jls/Kun/SelectItemTest.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jls.Kun;
 
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Yee Kun
- */
 public class SelectItemTest {
     
     public SelectItemTest() {
@@ -30,7 +21,6 @@ public class SelectItemTest {
         int expResult = 220;
         int result = Cal_subtotal(price, qty);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
     }
     
     /**
